372365e009d2b0c95f86c32533798947f416db70
Subash Arumugam <subasharumugam28@gmail.com>
2026-06-20 03:20:53 +0530

    timeout issue fixed

--- src/Api/Axios.js
+++ src/Api/Axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 const api=axios.create({
     baseURL:"https://vibepost-socialmediaapp-2.onrender.com",
-    timeout:10000,
+    // timeout:10000,
 })
 export default api
